d21c0841f7d75ddf325914eb7c8ff09640b86958
anag17 <nagya@carleton.edu>
2017-09-09 14:02:51 +0200

    Enemy Superclass start

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float base_speed;
+	public float run_speed;
+	public bool defeatable;
+	public float health;
+	public float point_value;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (-1,0,0) * Time.deltaTime * (base_speed + run_speed);
+	}
+}
