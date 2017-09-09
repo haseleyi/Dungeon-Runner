d7e7643fe12aa61179e5aef5ac99acee3bd3b398
Isaac Haseley <isaac.haseley@gmail.com>
2017-09-09 13:47:37 +0200

    Spawners

---
+++ Assets/Scripts/Spawners/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	protected System.Random random = new System.Random();
+		
+	protected void SpawnRandomPrefab(params GameObject[] prefabs) {
+		GameObject prefab = prefabs [random.Next (prefabs.Length)];
+		float location = GameManager.laneLocations [random.Next (GameManager.laneLocations.Count)];
+		Instantiate (prefab, new Vector3 (17, location, 0), Quaternion.identity);
+	}
+
+	protected void SpawnPrefab(GameObject prefab) {
+		float location = GameManager.laneLocations [random.Next (GameManager.laneLocations.Count)];
+		Instantiate (prefab, new Vector3 (17, location, 0), Quaternion.identity);
+	}
+}
