5dc43654c9aa31568fecaf94606551fb78e90cc8
anag17 <nagya@carleton.edu>
2017-09-14 01:24:37 +0200

    deleted pointless comment

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,7 +13,6 @@ public class PlayerController : MonoBehaviour {
 	Rigidbody2D body;
 	PlayerClass currentClass;
 	public static PlayerController instance;
-	//ScoreManager scoreManager = new ScoreManager();
 
 	void Start () {
 		instance = this;
