ec9cda1abd9821b9ded6bc84bd2c5bfc2110f331
Isaac Haseley <isaac.haseley@gmail.com>
2017-09-23 14:50:26 +0200

    Score tally

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
-	public Text coinsText;
-	private int coins;
+	public Text coinsText, totalScore;
 	public static ScoreManager instance;
+	int coins;
+	int score;
 
 	// Use this for initialization
 	void Start () {
 		instance = this;
 		coins = 0;
+		score = 0;
+		StartCoroutine (DistanceScoreCoroutine ());
 		UpdateCoins ();
+	}
+
+	void Update() {
+		totalScore.text = "Score: " + score.ToString ();
 	}
 
 	public void AddCoins (int numCoins) {
@@ -23,5 +30,13 @@ public class ScoreManager : MonoBehaviour {
 
 	void UpdateCoins () {
 		coinsText.text = coins.ToString();
+	}
+
+	IEnumerator DistanceScoreCoroutine() {
+		while (true) {
+			yield return new WaitForSeconds (.1f);
+			score++;
+			print ("Increased score");
+		}
 	}
 }
