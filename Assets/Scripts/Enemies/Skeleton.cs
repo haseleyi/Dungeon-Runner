cfb1c9e3918b4b25c2049460410fa4b9a3d5d7a5
Samuel Nozaki <samuel.nozaki@gmail.com>
2017-09-24 02:01:57 +0200

    Created Skeleton enemy class

---
+++ Assets/Scripts/Enemies/Skeleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : Enemy {
+
+	public float arrowCooldown;
+	
+	// Update is called once per frame
+	override public void FixedUpdate () {
+		if (transform.position.x < LaneManager.instance.xThreshold) {
+			Destroy (gameObject);
+		}
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	IEnumerator ArrowCooldown () {
+		
+	}
+}
