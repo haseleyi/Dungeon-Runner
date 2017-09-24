ad9f2a8593c6197ee9166418dbbbe861741e9bbb
Isaac Haseley <isaac.haseley@gmail.com>
2017-09-24 20:04:02 +0200

    Managed lanes for tank spawn

--- Assets/Scripts/Managers/LaneManager.cs
+++ Assets/Scripts/Managers/LaneManager.cs
@@ -15,27 +15,41 @@ public class LaneManager : MonoBehaviour {
 		instance = this;
 	}
 
-	public IEnumerator DisableSpawningCoroutine(float location) {
+	public IEnumerator DisableSpawningCoroutine(float location, bool tank) {
 		int laneIndex = laneLocations.IndexOf (location);
 		laneIsOpen [laneIndex] = false;
+		if (tank) {
+			laneIsOpen [laneIndex + 1] = false;
+		}
 		yield return new WaitForSeconds (spawnDelay);
 		laneIsOpen [laneIndex] = true;
+		if (tank) {
+			laneIsOpen [laneIndex + 1] = true;
+		}
 	}
 
 	public bool IsFreeLane() {
-		foreach (bool status in laneIsOpen) {
-			if (status) {
+		return laneIsOpen.Contains (true) ? true : false;
+	}
+
+	public bool TwoAdjacentLanesFree() {
+		for (int i = 0; i <= laneIsOpen.Count - 2; i++) {
+			if (laneIsOpen[i] && laneIsOpen[i + 1]) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	public float GetFreeLane() {
+	public float GetFreeLane(bool tank) {
 		System.Random random = new System.Random ();
 		int laneIndex;
 		while (true) {
-			laneIndex = random.Next (laneLocations.Count);
+			if (tank) {
+				laneIndex = random.Next (laneLocations.Count - 1);
+			} else {
+				laneIndex = random.Next (laneLocations.Count);
+			}
 			if (laneIsOpen[laneIndex]) {
 				return laneLocations[laneIndex];
 			}
--- Assets/Scripts/Spawners/Spawner.cs
+++ Assets/Scripts/Spawners/Spawner.cs
@@ -9,17 +9,25 @@ public class Spawner : MonoBehaviour {
 	protected void SpawnRandomPrefab(params GameObject[] prefabs) {
 		if (LaneManager.instance.IsFreeLane()) {
 			GameObject prefab = prefabs [random.Next (prefabs.Length)];
-			float location = LaneManager.instance.GetFreeLane();
+			float location = LaneManager.instance.GetFreeLane (false);
 			Instantiate (prefab, new Vector2 (17, location), Quaternion.identity);
-			StartCoroutine (LaneManager.instance.DisableSpawningCoroutine(location));
+			StartCoroutine (LaneManager.instance.DisableSpawningCoroutine(location, false));
 		}
 	}
 
 	protected void SpawnPrefab(GameObject prefab) {
 		if (LaneManager.instance.IsFreeLane()) {
-			float location = LaneManager.instance.GetFreeLane();
+			float location = LaneManager.instance.GetFreeLane(false);
 			Instantiate (prefab, new Vector2 (17, location), Quaternion.identity);
-			StartCoroutine (LaneManager.instance.DisableSpawningCoroutine(location));
+			StartCoroutine (LaneManager.instance.DisableSpawningCoroutine(location, false));
+		}
+	}
+
+	protected void SpawnTank(GameObject prefab) {
+		if (LaneManager.instance.TwoAdjacentLanesFree()) {
+			float location = LaneManager.instance.GetFreeLane (true);
+			Instantiate (prefab, new Vector2 (17, location), Quaternion.identity);
+			StartCoroutine (LaneManager.instance.DisableSpawningCoroutine(location, true));
 		}
 	}
 }
