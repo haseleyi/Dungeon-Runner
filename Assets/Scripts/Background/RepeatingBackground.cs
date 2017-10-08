1d00210621758d69ef04d4776ff5cfe0bcff7f51
Samuel Nozaki <samuel.nozaki@gmail.com>
2017-10-08 13:39:28 +0200

    Removed unused sprite variable in RepeatingBackground

--- Assets/Scripts/Background/RepeatingBackground.cs
+++ Assets/Scripts/Background/RepeatingBackground.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class RepeatingBackground : MonoBehaviour {
 
-	private SpriteRenderer sprite;
 	private float backgroundHorizontalLength;
 
 	void Awake () {
-		sprite = GetComponent<SpriteRenderer> ();
 		backgroundHorizontalLength = 17.8f;
 	}
 
@@ -26,5 +24,5 @@ public class RepeatingBackground : MonoBehaviour {
 		Vector2 backgroundOffSet = new Vector2(backgroundHorizontalLength, 0);
 		transform.position = (Vector2)transform.position+ backgroundOffSet*2f;
 	}
-		
+
 }
